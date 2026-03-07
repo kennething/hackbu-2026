b39b0f6703a86937b666f0ffb55492cc54001ee2
cow <Tiffanylin511@gmail.com>
2026-03-07 15:11:22 -0500

    Freat: returns JSONResponse of the class,confidence, and bounding box coord of images that turn through yolo model

--- backend/api/main.py
+++ backend/api/main.py
@@ -1,9 +1,15 @@
 from fastapi import FastAPI
 import cv2
+import base64
+from fastapi.responses import JSONResponse
+from pydantic import BaseModel
 import numpy as np
 from ultralytics import YOLO
 
 app = FastAPI()
+
+class ImageRequest(BaseModel):
+    image: str
 
 model = YOLO("letterdetector.pt")
 
@@ -13,13 +19,20 @@ def convert_base64_to_image(base64_string):          # converts JSON Blob/Base64
     img = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
     return img
 
-@app.post("/detect/letters/")
+
+@app.post("/detect")
 async def detect_letters(data: ImageRequest):
 
  img = convert_base64_to_image(data.image)
  results = model.predict(img)
+ detections =[]
+ for r in results:
+    for box in r.boxes:        # iterates through the detected boxes and extracts the class, confidence, and bounding box coordinates and adding it to detections list as a dictionary
+       detections.append({              
+        "class": int(box.cls),
+        "confidence": float(box.conf),
+        "x_min": int(box.xyxy.toList()),
+       })
  
-
  
- 
- return JSONResponse(content={"detections": })
+ return JSONResponse(content={"detections": detections})
